0ee210fb3d6d9f33dcd0af5ab2edd14aaec92e43
zenzawa <madhavkage@gmail.com>
2026-02-08 12:43:35 +0530

    fix swayosd style

--- home/notification.nix
+++ home/notification.nix
@@ -1,10 +1,12 @@
-{userpath, ...}: {
+{...}: {
   home.file."config/swaync/config.json".source = ../dots/swaync/config.json;
   home.file."config/swaync/style.css".source = ../dots/swaync/style.css;
   home.file."config/swaync/style" = {
     source = ../dots/swaync/style;
     recursive = true;
   };
+
+  home.file.".config/swayosd/style.css".source = ../dots/swayosd.css;
 
   services.swaync = {
     enable = true;
@@ -13,7 +15,5 @@
   services.swayosd = {
     enable = true;
     topMargin = 0.9;
-
-    stylePath = "${userpath}/flake/dots/swayosd.css";
   };
 }
